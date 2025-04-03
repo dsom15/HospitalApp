9c99ea6d5ed87cc1a80f1e44a667d37e24f4702c
Juan Esteban Vera Velez <juanestebanv0612@gmail.com>
2025-04-03 11:03:10 -0500

    Creo clases derivadas de medicamento y Medicamento Generico con su constuctor y su método para calcular el precio de venta

---
+++ src/autonoma/hospitalapp/models/MedicamentoGenerico.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.hospitalapp.models;
+
+/**
+ * Clase que contiene los medicamentos Genericos 
+ * Del Módulo de Farmarcia
+ * @author Juan Esteban Vera Velez
+ * Versión 1.0
+ * @since 2025-04-02
+ */
+  public class MedicamentoGenerico extends Medicamento {
+    /**
+     * Constructor de la clase MedicamentoGenerico.
+     * 
+     * @param nombre Nombre del medicamento.
+     * @param descripcion Descripción del medicamento.
+     * @param costo Costo del medicamento.
+     */
+    public MedicamentoGenerico(String nombre, String descripcion, double costo) {
+        super(nombre, descripcion, costo);
+    }
+
+    /**
+     * Calcula el precio de venta sumando un 10% al costo.
+     */
+    @Override
+    protected void calcularPrecioVenta() {
+        super.precioVenta = super.getCosto() * 1.10;
+    }
+}
