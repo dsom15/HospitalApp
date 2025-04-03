123a33d23bea8238be303a20ec4aee8af48d7344
Juan Esteban Vera Velez <juanestebanv0612@gmail.com>
2025-04-03 14:32:08 -0500

    Agrego InventarioFarmacia atributos y constructor

--- src/autonoma/hospitalapp/models/InventarioFarmacia.java
+++ src/autonoma/hospitalapp/models/InventarioFarmacia.java
@@ -4,10 +4,29 @@
  */
 package autonoma.hospitalapp.models;
 
-/**
- *
- * @author USER
+import java.util.ArrayList;
+import java.util.List;
+
+/** Clase que gestiona el inventario de la farmarcia del Hospital
+ * Contiene los medicamentos manejados por el Hospital
+ * @author Juan Esteban Vera Velez
+ * Versión 1.0
+ * @since 2025-04-03
  */
-public class InventarioFarmacia {
+
+class InventarioFarmacia {
     
+    /** Lista de medicamentos en el inventario */
+    private List<Medicamento> medicamentos;
+    
+    /** Año de actualización del inventario */
+    private int añoActualizacion;
+
+    /**
+     * Constructor de la clase InventarioFarmacia.
+     */
+    public InventarioFarmacia() {
+        this.medicamentos = new ArrayList<>();
+        this.añoActualizacion = 2025;
+    }
 }
