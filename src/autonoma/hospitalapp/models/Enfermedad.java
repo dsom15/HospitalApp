71e1ea3522df99066a59ddf1279174b3308904de
Rafael Arango <rafa131842@gmail.com>
2025-04-03 21:10:03 -0500

    Se crea la enfermedad dando solucion a varios problemas entre clases

---
+++ src/autonoma/hospitalapp/models/Enfermedad.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.hospitalapp.models;
+
+/**
+ *@since 03/04/2025
+ * versión 1.0
+ * @author Rafael
+ */
+public class Enfermedad {
+    private String nombre;
+    private String descripcion;
+
+    public Enfermedad(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String obtenerDescripcion() {
+        return this.descripcion;
+    }
+}
